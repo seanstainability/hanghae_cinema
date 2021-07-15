7ccf9aa78d3e520d108b8637f389ffe473f47890
seanstainability <seanstainability@gmail.com>
2021-07-15 13:41:35 +0900

    infinite

--- hanghae_cinema/src/redux/modules/movieSlice.js
+++ hanghae_cinema/src/redux/modules/movieSlice.js
@@ -20,7 +20,8 @@ const movieSlice = createSlice({
         // state.list = action.payload; // 서버 연동 테스트 하면서 수정 함 리뷰 필요
         // state.list = action.payload[0];
         // console.log(action.payload[0]);
-        state.list = [...(state.list || []), ...action.payload[0]];
+        // state.list = [...(state.list || []), ...action.payload[0]];
+        state.list.push(...action.payload[0]);
         state.paging = action.payload[1];
       })
       .addCase(getMovies.rejected, (state, action) => {
